aa65d21b6fed86b0e1976d84a6bf8401f2ea10ab
Hector David Taticuan <105394300+HectorTH1994@users.noreply.github.com>
2022-10-15 09:08:29 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -10,12 +10,27 @@ espacio entre palabra y palabra.
 
 """
 import pandas as pd
-
+import numpy as np
 
 def ingest_data():
 
     #
     # Inserte su código aquí
     #
+    
+    
+    df=pd.read_fwf('clusters_report.txt',
+                    on_bad_lines='skip',
+                    #colspecs="infer",
+                    #names=column_names,
+                     widths=[9, 15, 15,70])
+    df.columns=('cluster', 'cantidad de palabras clave', 'porcentaje de palabras clave', 'principales palabras clave')
+    df=df.drop([0, 1],axis=0).reset_index(drop=True)
+    
+    df = pd.DataFrame(df)
+    for i in range(len(df['cluster'])):
+        if df['cluster'][i]== pd.NAn:
+            print('null')
+    return df.head(5)
 
-    return df
+print(ingest_data())
